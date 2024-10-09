a712d4edf7877e84d1b6bf466dd3454cdb1e0acf
Kristers Laganovskis <58227978+kristersla@users.noreply.github.com>
2024-10-09 11:04:18 +0300

    Update main.cpp

--- UNI/Algoritmi un programmēšana/MD/C++/B/main.cpp
+++ UNI/Algoritmi un programmēšana/MD/C++/B/main.cpp
@@ -1,34 +1,58 @@
+/*******************************************
+Kristers Laganovskis, kl24033
+B19. Atrast visus simetriskus skaitļus intervālā [n, m], kurus kāpinot kvadrātā iegūst simetrisku skaitli.
+Skaitļu dalīšana ciparos jāveic skaitliski. Risinājumā izmantot funkciju, kas noskaidro, vai skaitlis ir
+simetrisks (Piemēram, skaitlim 2341 simetriskais skaitlis ir 1432).
+Programma izveidota: 30.09.2024.
+*******************************************/
 #include <iostream>
-
 using namespace std;
 
-void simetrisks(int skaitlis) {
+// Funkcija, kas pārbauda, vai skaitlis ir simetrisks.
+// Tā veic skaitļa apgriešanu un pārbauda, vai tas ir vienāds ar sākotnējo skaitli.
+void simetrisks(int skaitlis){
+
     int kapinats_skaitlis = skaitlis;
     int atmuguriski = 0;
 
-    do {
+    // cikls, kas sadala skaitli pa cipariem un apgriež to.
+    do{
         int pedejais_cipars = skaitlis % 10;
         atmuguriski = atmuguriski * 10 + pedejais_cipars;
         skaitlis = skaitlis / 10;
-    } while (skaitlis > 0);
+    }while (skaitlis > 0);
 
+    // Ja sākotnējais skaitlis ir vienāds ar apgriezto, tas ir simetrisks un izvada lieotājam paziņojumu.
     if (kapinats_skaitlis == atmuguriski) {
-        cout<< kapinats_skaitlis <<" ir simetrisks skaitlis kapinot ar 2"<<endl;
+        cout<<"Simetrisks skaitlis: "<<kapinats_skaitlis<<endl;
     }
 }
 
-int main() {
-    int n, m;
+int main(){
 
-    cout << "Ievadi pirmo skaitli: " << endl;
-    cin >> n;
-    cout << "Ievadi otro skaitli: " << endl;
-    cin >> m;
+	int ievade = 1;
 
-    for (int skaitlis = n; skaitlis <= m; skaitlis++) {
-        int kapinats_skaitlis = skaitlis * skaitlis;
-        simetrisks(kapinats_skaitlis);
-    }
+	do{
+		int n, m;
 
-    return 0;
+		// Lietotājam tiek lūgts ievadīt intervāla sākuma un beigu vērtības (n,m)
+		cout<<"Ievadi pirmo skaitli: " <<endl;
+		cin>>n;
+		cout<<"Ievadi otro skaitli: " <<endl;
+		cin>>m;
+
+		// Cikls, kas kāpina katru skaitli intervālā [n,m], pārbauda katru kāpināto skaitli vai, tas ir simetrisks.
+		for (int skaitlis = n; skaitlis <= m; skaitlis++) {
+
+			int kapinats_skaitlis = skaitlis * skaitlis;
+			simetrisks(kapinats_skaitlis);
+		}
+
+		// Tiek jautāts lietotājam vai vēlas turpināt programmu.
+		cout<<"Ievadi (1), ja velies turpinat, ievadi (0), ja velies beigt programmu: "<<endl;
+		cin>>ievade;
+
+	}while(ievade==1);
+
+	return 0;
 }
